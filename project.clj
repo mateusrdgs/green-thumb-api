a75569fecb292faf9c1c19f4f356e8ed6f31b1a5
Mateus Rodrigues <devrdgs@gmail.com>
2020-04-30 02:38:29 -0300

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+ (defproject green-thumb-api "0.1.0-SNAPSHOT"
+   :description "FIXME: write description"
+   :dependencies [[org.clojure/clojure "1.10.0"]
+                  [metosin/compojure-api "2.0.0-alpha30"]]
+   :ring {:handler green-thumb-api.handler/app}
+   :uberjar-name "server.jar"
+   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
+                   :plugins [[lein-ring "0.12.5"]]}})
