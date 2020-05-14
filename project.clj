910b3e684cb86d40fa12790080e2184af46fa8a0
Mateus Rodrigues <devrdgs@gmail.com>
2020-05-13 22:09:08 -0300

    chore: added gitignore

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
  (defproject green-thumb-api "0.1.0-SNAPSHOT"
    :description "FIXME: write description"
    :dependencies [[org.clojure/clojure "1.10.0"]
-                  [metosin/compojure-api "2.0.0-alpha30"]]
-   :ring {:handler green-thumb-api.handler/app}
+                  [metosin/compojure-api "2.0.0-alpha30"]
+                  [org.clojure/java.jdbc "0.7.11"]
+                  [mysql/mysql-connector-java "8.0.20"]]
+   :ring {:handler green-thumb-api.core/app}
    :uberjar-name "server.jar"
    :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
                    :plugins [[lein-ring "0.12.5"]]}})
